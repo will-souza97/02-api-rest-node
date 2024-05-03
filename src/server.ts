aaf4a2a94f34b6585db04ac29a10956353ce708c
will-souza97 <rzy.x97@gmail.com>
2024-05-02 22:26:52 -0300

    chore: config env

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => console.log('HTTP Server Running!'))
+app
+  .listen({ port: env.PORT })
+  .then(() => console.log(`HTTP Server Running! in ${env.PORT}`))
