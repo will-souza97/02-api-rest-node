6a0b38b171233370e76fd8b093b613e861ed906f
will-souza97 <rzy.x97@gmail.com>
2024-05-02 22:44:22 -0300

    chore: add host

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,8 @@ import { app } from './app'
 import { env } from './env'
 
 app
-  .listen({ port: env.PORT })
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
   .then(() => console.log(`HTTP Server Running! in ${env.PORT}`))
