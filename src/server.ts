f77dfb6fcccf6b15f23acde5fd49fae2a0603c99
will-souza97 <rzy.x97@gmail.com>
2024-05-02 22:38:24 -0300

    test: repalce port

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { app } from './app'
 import { env } from './env'
 
 app
-  .listen({ port: env.PORT })
+  .listen({ port: Number(process.env.PORT) || 10001 })
   .then(() => console.log(`HTTP Server Running! in ${env.PORT}`))
